2b9635da9f2e00638e18c915c7d1e292dbb3108c
azucat2 <azucat2@users.noreply.github.com>
2026-01-31 02:17:26 +0900

    fix format

--- youtube_remove_playlist_parameter.user.js
+++ youtube_remove_playlist_parameter.user.js
@@ -12,11 +12,11 @@
 // @license      MIT
 // ==/UserScript==
 
-(function(){
-    var url = document.URL;
-    if (url.indexOf("about:blank") == -1) {
-        if(url.match(/\?v=.{11}&list=PL.{32}&index=[0-9]*/)) {
-            location.href = url.replace(/&list=PL.{32}&index=[0-9]*/, "");
-        }
+(function () {
+  var url = document.URL;
+  if (url.indexOf("about:blank") == -1) {
+    if (url.match(/\?v=.{11}&list=PL.{32}&index=[0-9]*/)) {
+      location.href = url.replace(/&list=PL.{32}&index=[0-9]*/, "");
     }
+  }
 })();
