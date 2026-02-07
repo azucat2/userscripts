d4ac1fc0a837141aa62670ee8698feb0d9f5dafc
azucat2 <azucat2@users.noreply.github.com>
2026-02-08 03:13:09 +0900

    update YouTube Remove playlist parameters

--- youtube_remove_playlist_parameter.user.js
+++ youtube_remove_playlist_parameter.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         YouTube Remove playlist parameters
 // @namespace    https://github.com/azucat2
-// @version      1.0.0
+// @version      1.1.0
 // @updateURL    https://github.com/azucat2/userscripts/raw/main/youtube_remove_playlist_parameter.user.js
 // @downloadURL  https://github.com/azucat2/userscripts/raw/main/youtube_remove_playlist_parameter.user.js
 // @description  Remove the playlist parameters from the YouTube url.
 // @author       azucat2
+// @match        https://www.youtube.com/playlist*
 // @match        https://www.youtube.com/watch*
 // @run-at       document-start
 // @grant        none
@@ -13,10 +14,54 @@
 // ==/UserScript==
 
 (function () {
-  var url = document.URL;
-  if (url.indexOf("about:blank") == -1) {
-    if (url.match(/\?v=.{11}&list=PL.{32}&index=[0-9]*/)) {
-      location.href = url.replace(/&list=PL.{32}&index=[0-9]*/, "");
+  "use strict";
+
+  function getCleanUrl(url) {
+    if (
+      typeof url !== "string" ||
+      !url.includes("/watch") ||
+      !url.includes("list=")
+    ) {
+      return null;
     }
+
+    try {
+      const urlObj = new URL(url, window.location.origin);
+      urlObj.searchParams.delete("list");
+      urlObj.searchParams.delete("index");
+      return urlObj.toString();
+    } catch {
+      return null;
+    }
+  }
+
+  window.addEventListener(
+    "yt-navigate-start",
+    (event) => {
+      const detail = event.detail;
+      if (!detail) return;
+
+      const originalUrl =
+        detail.url || detail.endpoint?.commandMetadata?.webCommandMetadata?.url;
+      const cleanUrl = getCleanUrl(originalUrl);
+
+      if (cleanUrl) {
+        event.preventDefault();
+        event.stopImmediatePropagation();
+
+        const ytdApp = document.querySelector("ytd-app");
+        if (ytdApp && typeof ytdApp.navigate === "function") {
+          ytdApp.navigate(cleanUrl);
+        } else {
+          window.location.assign(cleanUrl);
+        }
+      }
+    },
+    true,
+  );
+
+  const cleanInitial = getCleanUrl(window.location.href);
+  if (cleanInitial) {
+    window.location.replace(cleanInitial);
   }
 })();
